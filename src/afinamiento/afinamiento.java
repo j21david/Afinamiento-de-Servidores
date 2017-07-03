370fa0138b872a2a64bf5f68c9ae1de0bd8d6f25
Kevin.Jimenez24 <kivito2009@hotmail.com>
2017-07-03 04:33:14 -0500

    Version 4

--- src/afinamiento/afinamiento.java
+++ src/afinamiento/afinamiento.java
@@ -11,8 +11,7 @@ import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import server.MiConsola;
+
 
 
 
